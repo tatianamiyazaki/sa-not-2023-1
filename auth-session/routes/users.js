6fbfa5b63c44c70a29f6b311ec4519a36d06c9b3
tatianamiyazaki <tatimiyazaki@gmail.com>
2023-03-24 01:13:45 +0000

    (22/03) continua auth-session

--- auth-session/routes/users.js
+++ auth-session/routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const controller = require('../controllers/users')
 
 /* GET users listing. */
-router.get('/form', controller.formNew);
+router.get('/form', controller.formNew)
+router.get('/login', controller.formLogin)
+router.post('/', controller.create)
+router.post('/auth', controller.auth)
 
 module.exports = router;
